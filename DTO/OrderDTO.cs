99b040536747c6bf15fa85c48b9e24010d10b406
Zeryuix <leo.mermet91@gmail.com>
2021-03-16 16:02:08 +0000

    first commit

---
+++ DTO/OrderDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.DTO
+{
+    public class OrderDTO
+    {
+        public int Customer_id { get; set; }
+        public List<int> Products { get; set; }
+    }
+}
